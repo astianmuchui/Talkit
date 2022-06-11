6e1936e100f446edbc38a7998e1c57469b2c5665
astian4 <sebastianmuchui79@gmail.com>
2022-06-11 11:35:18 +0300

    building

---
+++ UI/js/err_triggers.js
@@ -0,0 +1,6 @@
+let span = document.getElementById("close");
+let error = document.getElementById("error");
+span.onclick = ()=>{
+    console.log("identified");
+    error.style.display = "none";
+}
