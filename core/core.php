6801821b26ab7a3e553431a3a57850392bf328a6
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-02 19:35:54 +0300

    Added procedural SQL

--- core/core.php
+++ core/core.php
@@ -10,6 +10,73 @@
 use FFI\Exception;
 
 session_start();
+
+//  Procedural SQL in case i need it for whatever weird reason
+class sequel {
+   protected $username = 'root';
+   protected $host = "localhost";
+   protected $pwd = "";
+   protected  $dbname;
+   protected $connection;
+   protected $result;
+   protected $query;
+   protected $table;
+   protected $data;
+   protected $number;
+   protected $id;
+
+   protected function establish_connection(){
+      return $this->connection = mysqli_connect($this->host,$this->username,$this->pwd,$this->dbname);
+   }
+   protected function close_connection(){
+      return mysqli_close(self::establish_connection($this->dbname));
+   }
+   protected function query($query){
+      return $this->result = mysqli_query(self::establish_connection($this->dbname),$this->query);
+
+   }
+   protected function fetch_all_assoc($table){
+      $this->query = "SELECT * FROM `$this->table` ORDER BY id DESC";
+      return $this->data = mysqli_fetch_all(self::query($this->dbname,$this->query),MYSQLI_ASSOC);
+      return mysqli_free_result(self::query($this->dbname,$this->query));
+      self::close_connection();
+   }
+   protected function fetchArray($table,$column){
+      $this->query = "SELECT $column FROM `$this->table` ORDER BY id DESC";
+      return $this->data = mysqli_fetch_all(self::query($this->dbname,$this->query),MYSQLI_ASSOC);
+      return mysqli_free_result(self::query($this->dbname,$this->query));
+      self::close_connection();
+   }
+   protected function fetch_newest($table,$number){
+      $this->query = "SELECT * FROM `$this->table` ORDER BY id DESC LIMIT $this->number";
+      return $this->data =  mysqli_fetch_all(self::query($this->dbname,$this->query));
+      mysqli_free_result(self::query($this->dbname,"SELECT * FROM `$this->table` ORDER BY id DESC LIMIT $this->number"));
+      self::close_connection();
+   }
+   protected function fetch_oldest($table,$number){
+      $this->query = "SELECT * FROM `$this->table` ORDER BY id ASC LIMIT $this->number";
+      return $this->data =  mysqli_fetch_all(self::query($this->dbname,$this->query));
+      mysqli_free_result(self::query($this->dbname,"SELECT * FROM `$this->table` ORDER BY id ASC LIMIT $this->number"));
+      self::close_connection();
+   }
+   protected function count($dbname,$table){
+      self::establish_connection($dbname);
+      $this->query = "SELECT * FROM $this->table";
+      $salt = self::query($dbname,$this->query);
+      return $rows_count = count(mysqli_fetch_all($salt,MYSQLI_ASSOC));
+      mysqli_free_result($salt);
+      self::close_connection();
+   }
+   protected function fetch_single($table,$id){
+      $this->query = "";
+      return $this->data = mysqli_fetch_assoc(self::query($this->dbname,$this->query),MYSQLI_ASSOC);
+      mysqli_free_result(self::query($this->dbname,"SELECT * FROM $this->table WHERE id = $this->id"));
+      self::close_connection();
+   }
+}
+
+// PDO SQL :
+
 class
  Database
 {
