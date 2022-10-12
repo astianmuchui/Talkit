eb885c4544d02c7e3339c6084eda4df7174d3398
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-12 06:32:19 +0300

    Updated

--- core/xhr/requests.php
+++ core/xhr/requests.php
@@ -1,8 +1,16 @@
 <?php
+   if($_SERVER["SERVER_REQUEST_METHOD"]!=="GET"){
+      http_response_code(403);
+      exit;
+
+
+   }else{
    $str = $_GET['search'];
    if($str){
       include "../core.php";
       $ops = new Operations;
       $ops->search($str);
    }
+   }
+
 ?>
